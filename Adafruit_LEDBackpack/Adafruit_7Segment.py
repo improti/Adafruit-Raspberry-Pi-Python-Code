3b91186443f9b27e713fa94240eaa703ebe16d53
improti <tim@dirasad.de>
2015-10-12 20:09:48 +0200

    added function to invert the display

--- Adafruit_LEDBackpack/Adafruit_7Segment.py
+++ Adafruit_LEDBackpack/Adafruit_7Segment.py
@@ -27,19 +27,34 @@ class SevenSegment:
   mask_colons = 0xFFFF
  
   # Hexadecimal character lookup table (row 1 = 0..9, row 2 = A..F)
-  digits = [ 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, \
-             0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 ]
-
+  DIGITS_NORMAL = [ 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, \
+                    0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 ]
+  DIGITS_INVERTED = [ 0x3F, 0x30, 0x5B, 0x79, 0x74, 0x6D, 0x6F, 0x38, 0x7F, 0x7D, \
+                      0x7E, 0x67, 0x0F, 0x73, 0x4F, 0x4E ]
+  digits = DIGITS_NORMAL
+  display_inverted = False
+  
   # Constructor
   def __init__(self, address=0x70, debug=False):
     if (debug):
       print "Initializing a new instance of LEDBackpack at 0x%02X" % address
     self.disp = LEDBackpack(address=address, debug=debug)
 
+  def invertDisplay(self):
+    if (self.display_inverted): # we were inverted, so switch back to normal
+      self.digits = self.DIGITS_NORMAL
+      self.display_inverted = False
+    else:                       # we were displaying normally, so switch to inverted
+      self.digits = self.DIGITS_INVERTED
+      self.display_inverted = True
+  
   def writeDigitRaw(self, charNumber, value):
     "Sets a digit using the raw 16-bit value"
     if (charNumber > 7):
       return
+     if ((self.display_inverted) & (charNumber < 5)):
+      "if inverted we need to use reverse character positioning"
+      charNumber = 4 - charNumber
     # Set the appropriate digit
     self.disp.setBufferRow(charNumber, value)
 
@@ -49,6 +64,9 @@ class SevenSegment:
       return
     if (value > 0xF):
       return
+    if ((self.display_inverted) & (charNumber < 5)):
+      "if inverted we need to use reverse character positioning"
+      charNumber = 4 - charNumber
     # Set the appropriate digit
     self.disp.setBufferRow(charNumber, self.digits[value] | (dot << 7))
 
